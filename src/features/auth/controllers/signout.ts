1b505701bb2865500fb045cfb35e3849b4cde91c
/belay1.sirak2.tsige3 <belay1.sirak2.tsige3@gmail.com>
2024-06-05 11:18:01 +0300

    chore: Refactor signout controller and add tests for signout functionality

--- src/features/auth/controllers/signout.ts
+++ src/features/auth/controllers/signout.ts
@@ -1,9 +1,9 @@
-import { Request, Response } from 'express';
-import HTTP_STATUS from 'http-status-codes';
+import { Request, Response } from "express";
+import HTTP_STATUS from "http-status-codes";
 
 export class signOut {
-    public update(req: Request, res: Response) {
-        req.session = null;
-        res.status(HTTP_STATUS.OK).json({ message: 'Sign out successful' });
-    }
+  public update(req: Request, res: Response) {
+    req.session = null;
+    res.status(HTTP_STATUS.OK).json({ message: "Sign out successful" });
+  }
 }
--- src/mocks/auth.mock.ts
+++ src/mocks/auth.mock.ts
@@ -1,29 +1,29 @@
-import { AuthPayload, IAuthDocument } from "@auth/interfaces/auth.interface";
-import { CurrentUser } from "../features/auth/controllers/current-user";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { Response } from "express";
+import { AuthPayload, IAuthDocument } from "@auth/interfaces/auth.interface";
+
 export const authMockRequest = (
   sessionData: IJWT,
   body: IAuthMock,
   currentUser?: AuthPayload | null,
   params?: any
-) => {
+) => ({
+  session: sessionData,
+  body,
+  params,
+  currentUser,
+});
+
+export const authMockResponse = (): Response => {
   const res: Response = {} as Response;
   res.status = jest.fn().mockReturnValue(res);
   res.json = jest.fn().mockReturnValue(res);
   return res;
 };
+
 export interface IJWT {
   jwt?: string;
-}
-export interface IAuthMock {
-  _id?: string;
-  username?: string;
-  email?: string;
-  uId?: string;
-  password?: string;
-  avatarColor?: string;
-  avatarImage?: string;
-  createdAt?: Date | string;
 }
 
 export interface IAuthMock {
